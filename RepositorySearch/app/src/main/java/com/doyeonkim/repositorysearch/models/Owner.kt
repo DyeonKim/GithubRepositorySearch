15cc4dafa6434bc3732c95796185bb28b7c415df
DyeonKim <vlxjdus@gmail.com>
2022-01-03 19:59:04 +0900

    #3 #5 fix : Data class, DataBinding 수정

--- RepositorySearch/app/src/main/java/com/doyeonkim/repositorysearch/models/Owner.kt
+++ RepositorySearch/app/src/main/java/com/doyeonkim/repositorysearch/models/Owner.kt
@@ -4,5 +4,5 @@ import com.google.gson.annotations.SerializedName
 
 data class Owner(
     @SerializedName("login") val userId: String,
-    val avatarUrl: String
+    @SerializedName("avatar_url")val avatarUrl: String
 )
--- RepositorySearch/app/src/main/java/com/doyeonkim/repositorysearch/models/Repo.kt
+++ RepositorySearch/app/src/main/java/com/doyeonkim/repositorysearch/models/Repo.kt
@@ -1,13 +1,3 @@
 package com.doyeonkim.repositorysearch.models
 
-import com.google.gson.annotations.SerializedName
-
-data class Repo(
-    @SerializedName("full_name") val name: String,
-    val language: String,
-    val owner: Owner,
-    @SerializedName("html_url") val url: String,
-    val description: String,
-    val forks: Int,
-    val watchers: Int,
-)
+data class Repo(val items: MutableList<Item>)
